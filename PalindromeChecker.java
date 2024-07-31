1675d9bfa40eab7f05d824670264ae664e669ecb
Edwin HC <edwinrhc@gmail.com>
2024-07-31 17:21:21 -0500

    Palindromo

--- PalindromeChecker.java
+++ PalindromeChecker.java
@@ -1,2 +1,30 @@
-public class index {
+import java.util.Scanner;
+
+public class PalindromeChecker {
+
+    /** Crear un programa que sea palíndromo/
+     *
+     */
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese palabra o frase");
+        String cadena = scanner.nextLine();
+
+        if(esPalindromo(cadena)){
+            System.out.println("Es palindromo");
+        }else{
+            System.out.println("No es palindromo");
+        }
+
+
+    }
+
+    public static boolean esPalindromo(String cadena){
+        String cadenaFrase = cadena.replaceAll("[^a-zA-Z0-9]","").toLowerCase();
+        return cadenaFrase.equals(new StringBuilder(cadenaFrase).reverse().toString());
+    }
+
+
+
+
 }
