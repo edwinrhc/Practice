605cacf0b9d3a43a8c2ac3014a1a5de6617472c4
Edwin HC <edwinrhc@gmail.com>
2024-08-01 17:29:06 -0500

    Example Wrapper

--- src/WrapperExample.java
+++ src/WrapperExample.java
@@ -11,6 +11,13 @@ public class WrapperExample {
         int sum = intObject1 + intObject2; // Unboxing de Integer a int
         System.out.println("La suma de " + intObject1+ " y " + intObject2 + " es: " + sum  );
 
+        // Ejemplo de uso de Double (Clase wrapper para double)
+        Double doubleObject1 = Double.valueOf(5.75); // Creación de un objeto Double
+        Double doubleObject2 = 2.25; // Autoboxing de double a Double
+
+        // Multiplicar dos Double (se realiza unboxing y luego la multiplicación)
+        double product = doubleObject1 * doubleObject2; // Unboxing de Double a double
+        System.out.println("El producto de " + doubleObject1 + " y " + doubleObject2 + " es: " +product);
 
     }
 }
