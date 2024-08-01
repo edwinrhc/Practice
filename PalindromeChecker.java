42507159b5c98c9158e254ba79f575f583b2ba09
Edwin HC <edwinrhc@gmail.com>
2024-08-01 08:40:00 -0500

    Palindromo

--- PalindromeChecker.java
+++ PalindromeChecker.java
@@ -5,27 +5,25 @@ public class PalindromeChecker {
     /** Crear un programa que sea palíndromo/
      *
      */
-    public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Ingrese palabra o frase");
-        String cadena = scanner.nextLine();
+  public static void main(String[] args){
+      Scanner scanner = new Scanner(System.in);
+      System.out.println("Ingrese la palabra o frase: ");
+      String cadena = scanner.nextLine();
 
-        if(esPalindromo(cadena)){
-            System.out.println("Es palindromo");
-        }else{
-            System.out.println("No es palindromo");
-        }
+      if(isPalindromo(cadena)){
+          System.out.println("Si palabra es palíndromo");
+      }else{
+          System.out.println("No es palabra palíndromo");
+      }
 
+  }
 
-    }
+  public static boolean isPalindromo(String cadena){
 
-    public static boolean esPalindromo(String cadena){
-        String cadenaFrase = cadena.replaceAll("[^a-zA-Z0-9]","").toLowerCase();
+      String frase = cadena.replaceAll("[^a-zA-Z0-9]","").toLowerCase();
+      return frase.equals(new StringBuilder(frase).reverse().toString());
 
-        return cadenaFrase.equals(new StringBuilder(cadenaFrase).reverse().toString());
-    }
-
-
+  }
 
 
 }
