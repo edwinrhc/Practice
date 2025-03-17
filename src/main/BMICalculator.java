803dbfcd3fe32a9a20a77e59904d1d1901964afc
Edwin HC <edwinrhc@gmail.com>
2025-03-17 16:11:12 -0500

    BMICalculator Test

--- src/main/BMICalculator.java
+++ src/main/BMICalculator.java
@@ -17,6 +17,7 @@ public class BMICalculator {
             */
     public static String bmi(double weight, double height) {
         double bmi = weight /(height * height); // Calcular BMI
+        String bmiFormatted = String.format("%.2f", bmi); // Formatear el BMI con dos decimales
         // Evaluar los rangos del BMI y devolver la categoría
         // 1 Opción
 /*        if(bmi  <= 18.5) return "Underweight";
@@ -25,15 +26,15 @@ public class BMICalculator {
         return "Obese";*/
 
         // 2 Opción
-        return (bmi <= 18.5) ? "Underweight"
-                : (bmi <= 25.0) ? "Normal"
-                : (bmi <= 30.0) ? "Overweight"
-                : "Obese";
+        return (bmi <= 18.5) ? bmiFormatted + ": Underweight"
+                : (bmi <= 25.0) ? bmiFormatted +": Normal"
+                : (bmi <= 30.0) ?  bmiFormatted + ": Overweight"
+                :  bmiFormatted + ": Obese";
     }
 
     public static void main(String[] args) {
-        System.out.println(bmi(80, 1.80));  // "Normal"
-        System.out.println(bmi(50, 1.70));  // "Underweight"
+        System.out.println(bmi(79, 1.67));  // "Normal"
+        System.out.println(bmi(60, 1.70));  // "Underweight"
         System.out.println(bmi(90, 1.75));  // "Overweight"
         System.out.println(bmi(110, 1.60)); // "Obese"
 
