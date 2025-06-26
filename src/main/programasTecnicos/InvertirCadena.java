f1626ba8fe98cf9c331b406e8be4ad20e1250981
Edwin HC <edwinrhc@gmail.com>
2025-06-26 18:04:25 -0500

    feat: validar que sean letras

--- src/main/programasTecnicos/InvertirCadena.java
+++ src/main/programasTecnicos/InvertirCadena.java
@@ -11,19 +11,30 @@ public class InvertirCadena {
         String texto = sc.nextLine();
         sc.close();
 
+        // Validar con regex
+        if (!texto.matches("^[A-Za-zñÑáéíóúÁÉÍÓÚ]+$")) {
+            System.out.println("Error: Solo se permiten letras (sin espacios, números ni caracteres especiales)");
+            return;
+        }
+
         System.out.println("Cadena original: " + texto);
         String invertida = invertir(texto);
         System.out.println("Cadena invertida: " + invertida);
 
     }
 
-    public static String invertir(String s) {
-        StringBuilder resultado = new StringBuilder();
-        for (int i = s.length() - 1; i >= 0; i--) {
-            resultado.append(s.charAt(i));
-        }
-        return resultado.toString();
-    }
+    // observado:Menos recomendada
+//    public static String invertir(String s) {
+//        StringBuilder resultado = new StringBuilder();
+//        for (int i = s.length() - 1; i >= 0; i--) {
+//            resultado.append(s.charAt(i));
+//        }
+//        return resultado.toString();
+//    }
 
+    //nota:Recomendada
+    public static String invertir(String s) {
+        return new StringBuilder(s).reverse().toString();
+    }
 
 }
